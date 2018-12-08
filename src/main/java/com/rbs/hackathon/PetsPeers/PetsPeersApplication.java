81779834e8bcdbeac527faf9dad28d893d54ce0b
arpitcool68 <arpitcool68@gmail.com>
2018-12-07 22:45:08 -0800

    Adding initial commit

---
+++ src/main/java/com/rbs/hackathon/PetsPeers/PetsPeersApplication.java
@@ -0,0 +1,12 @@
+package com.rbs.hackathon.PetsPeers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsPeersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsPeersApplication.class, args);
+	}
+}
