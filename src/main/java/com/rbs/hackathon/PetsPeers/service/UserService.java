d5250f674720566e5e28fb0ae75defc97c9c98b7
tabish <mohdtabish99@gmail.com>
2018-12-07 23:58:26 -0800

    added user service

---
+++ src/main/java/com/rbs/hackathon/PetsPeers/service/UserService.java
@@ -0,0 +1,21 @@
+package com.rbs.hackathon.PetsPeers.service;
+
+public interface UserService {
+	
+
+void addUser();
+
+void updateUser();
+
+void listUsers();
+
+void getUserById();
+
+void removeUser();
+
+void findByUserName();
+void buyPet();
+
+void getMyPets();
+
+}
---
+++ src/main/java/com/rbs/hackathon/PetsPeers/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.rbs.hackathon.PetsPeers.service;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void updateUser() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void listUsers() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getUserById() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeUser() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findByUserName() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void buyPet() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getMyPets() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
